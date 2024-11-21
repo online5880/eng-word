24a1f71e6fc6a640dbe66f616e6dd0b01af85de7
seungyeon <seongyon98@gmail.com>
2024-11-21 09:43:20 +0900

    스타일 변경

---
+++ spell_stars/static/js/testresults.js
@@ -0,0 +1,9 @@
+document.getElementById('againBtn').addEventListener('click', function() {
+    // 시험 모드 첫 화면으로 이동
+    window.location.href = '/test/'; // 적절한 경로로 수정
+});
+
+document.getElementById('exitBtn').addEventListener('click', function() {
+    // 홈 화면으로 이동
+    window.location.href = '/';
+});
